c9cea8ba9084328637e08bacdd33f8eb05d6418f
jdbravol <jdbravol@utexas.edu>
2017-04-16 23:23:13 -0500

    added main layout, login layout, andmain feed layout

--- app/src/main/java/com/example/ee461lproject/MainActivity.java
+++ app/src/main/java/com/example/ee461lproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ee461lproject;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setContentView(R.layout.activity_login);
     }
 }

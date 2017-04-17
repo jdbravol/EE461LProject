2d00dd590defaddd6303d33ee72568dc3455ec3b
Aaron Babber <aaron.a.babber@gmail.com>
2017-04-16 23:43:47 -0500

    Finished "Event Details" layout

--- app/src/main/java/com/example/ee461lproject/event_details.java
+++ app/src/main/java/com/example/ee461lproject/event_details.java
@@ -3,12 +3,13 @@ package com.example.ee461lproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class event_details extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_event_details);
         // TODO: Set title
+        // TODO: Set vertical scrolling for description
     }
 }

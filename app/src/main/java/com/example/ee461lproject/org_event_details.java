8de11467242b8d3b93ac93097ac5e9185b0e0d70
Aaron Babber <aaron.a.babber@gmail.com>
2017-04-22 18:12:01 -0500

    Made progress on tabbed layout for organization accounts

---
+++ app/src/main/java/com/example/ee461lproject/org_event_details.java
@@ -0,0 +1,13 @@
+package com.example.ee461lproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class org_event_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_event_details);
+    }
+}

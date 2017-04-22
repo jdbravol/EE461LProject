04071c1b525d610dd5c6d7a556abadd4d6f3e433
jdbravol <jdbravol@utexas.edu>
2017-04-22 18:13:14 -0500

    fixed main feed to include search bar

--- app/src/main/java/com/example/ee461lproject/MainActivity.java
+++ app/src/main/java/com/example/ee461lproject/MainActivity.java
@@ -1,21 +1,123 @@
 package com.example.ee461lproject;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private OrganizationOptions.SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // TODO: Set title
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_options);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create the adapter that will return a fragment for each of the two
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new OrganizationOptions.SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    // TODO: Replace default options with logout
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_organization_name, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        setContentView(R.layout.activity_login);
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // DELETED PlaceholderFragment
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    Tab1_CreateEvent tab1 = new Tab1_CreateEvent();
+                    return tab1;
+                case 1:
+                    Tab2_CreatedFeed tab2 = new Tab2_CreatedFeed();
+                    return tab2;
+                case 2:
+                    Tab3_SubscribedEvents tab3 = new Tab3_SubscribedEvents();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SEARCH";
+                case 1:
+                    return "MAIN";
+                case 2:
+                    return "SUBSCRIBED";
+            }
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/example/ee461lproject/Tab2_MainEvents.java
@@ -0,0 +1,8 @@
+package com.example.ee461lproject;
+
+/**
+ * Created by juanbravo on 4/18/17.
+ */
+
+public class Tab2_MainEvents {
+}

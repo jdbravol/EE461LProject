f39ab838a78ae0572015d1d36e08c792c44e14da
ezueckg <ezueckg@utexas.edu>
2017-04-22 19:53:59 -0500

    Created Event and Database classes

---
+++ app/src/main/java/com/example/ee461lproject/Database.java
@@ -0,0 +1,55 @@
+package com.example.ee461lproject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by ezuec on 4/22/2017.
+ */
+public class Database {
+
+    public static void updateEvent(Event event){
+
+    }
+
+    public static void addEvent(Event event){
+
+    }
+
+    public static ArrayList<Event> allFutureEvents(){
+        return null;
+
+    }
+
+    public static ArrayList<Event> allPastEvents(){
+        return null;
+    }
+
+    public static ArrayList<Event> allFutureEventsByOrg(String org){
+        return null;
+    }
+
+    public static ArrayList<Event> allPastEventsByOrg(String org){
+        return null;
+    }
+
+    public static ArrayList<Event> allEventsByOrg(String org){
+        return null;
+    }
+
+    public static ArrayList<Event> allRSVPEvents(String user){
+        return null;
+    }
+
+    public static ArrayList<Event> allPastRSVPEvents(String user){
+        return null;
+    }
+
+    public static ArrayList<Event> allFutureRSVPEvents(String user){
+        return null;
+    }
+}
+
+
+
+

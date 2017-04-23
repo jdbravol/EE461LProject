a63fb2fd0f95532fe6b0008e8ed3f425292ed3c1
ezueckg <ezueckg@utexas.edu>
2017-04-23 14:09:45 -0500

    Update on DB

--- app/src/main/java/com/example/ee461lproject/SplashActivity.java
+++ app/src/main/java/com/example/ee461lproject/SplashActivity.java
@@ -9,6 +9,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
 public class SplashActivity extends Activity{
     private final int SPLASH_DISPLAY_LENGTH = 5000;
 
@@ -18,6 +23,14 @@ public class SplashActivity extends Activity{
 
         setContentView(R.layout.activity_splash);
         super.onCreate(icicle);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference eventsRef = database.getReference("Events");
+
+        eventsRef.addChildEventListener(new EventListener());
+
+        Event event = new Event("TestEvent", "TestOrg", new Date(), "Test Location", "Test descript");
+        Database.makeEvent(event);
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/

a2b8e7af7e2275137303d61a5c90fec3188e5af1
jdbravol <jdbravol@utexas.edu>
2017-05-01 10:55:53 -0500

    -worked on layouts

---
+++ app/src/androidTest/java/com/example/ee461lproject/RegisteredUserTest.java
@@ -0,0 +1,83 @@
+package com.example.ee461lproject;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RegisteredUserTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
+
+    @Test
+    public void registeredUserTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3594962);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.emailLine), isDisplayed()));
+        appCompatEditText.perform(replaceText("jdbravol@utexas.edu"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.passwordLine), isDisplayed()));
+        appCompatEditText2.perform(replaceText("9565628092"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.loginButton), withText("Login"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3548388);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withText("MAIN"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withText("SUBSCRIBED"), isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        ViewInteraction appCompatTextView3 = onView(
+                allOf(withText("SEARCH"), isDisplayed()));
+        appCompatTextView3.perform(click());
+
+        ViewInteraction appCompatCheckBox = onView(
+                allOf(withId(R.id.checkBox), withText("Free Food"), isDisplayed()));
+        appCompatCheckBox.perform(click());
+
+        ViewInteraction appCompatCheckBox2 = onView(
+                allOf(withId(R.id.checkBox), withText("Free Food"), isDisplayed()));
+        appCompatCheckBox2.perform(click());
+
+    }
+
+}

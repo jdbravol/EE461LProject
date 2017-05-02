0c491623c1aa103f69ac0ca7d8e9efff55a2f284
jdbravol <jdbravol@utexas.edu>
2017-05-01 23:51:11 -0500

    fixed event detalis layout to be scrollable

--- app/src/main/java/com/example/ee461lproject/event_details.java
+++ app/src/main/java/com/example/ee461lproject/event_details.java
@@ -3,7 +3,6 @@ package com.example.ee461lproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import org.parceler.Parcels;
@@ -18,7 +17,7 @@ public class event_details extends AppCompatActivity {
         Intent i = getIntent();
         Event event = (Event) Parcels.unwrap(i.getParcelableExtra("EVENT"));
 
-        TextView title = (TextView) findViewById(R.id.eventNameStudent);
+        TextView title = (TextView) findViewById(R.id.eventNameOrg);
         title.setText(event.getEventName());
     }
 

1b2016b7d7f623e57d3584dc6a3356e315831da8
ezueckg <ezueckg@utexas.edu>
2017-05-01 22:12:47 -0500

    Made the Event parcelable.

--- app/src/main/java/com/example/ee461lproject/event_details.java
+++ app/src/main/java/com/example/ee461lproject/event_details.java
@@ -1,15 +1,20 @@
 package com.example.ee461lproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class event_details extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
-        // TODO: Set title
-        // TODO: Set vertical scrolling for description
+        Intent i = getIntent();
+        Event event = (Event) i.getParcelableExtra("EVENT");
+
+        TextView title = (TextView) findViewById(R.id.eventNameStudent);
+        title.setText(event.getEventName());
     }
 
 }
